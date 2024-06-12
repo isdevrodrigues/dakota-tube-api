e114a4e4d7ee52c0d4501d3da559bfece0ce1d5c
Allan <lumina9@proton.me>
2024-06-12 17:27:32 -0200

    changing cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 app.use(cors({
-    origin: 'https://your-github-username.github.io',
+    origin: 'https://dakota-tube.netlify.app/',
     optionsSuccessStatus: 200
 }));
 
